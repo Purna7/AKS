a1cccfc477e081af27e9ddfba053fbfc8e588ce3
purna7 <purnachandrar@gmail.com>
2025-12-09 14:36:10 +0530

    Added logic to provision ACR and assign required roles to pull image by AKS cluster

--- InfrastructureAsCode/main.tf
+++ InfrastructureAsCode/main.tf
@@ -69,3 +69,21 @@ resource "azurerm_kubernetes_cluster" "aks" {
 	]
 }
 
+// Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+	name                = var.acr_name
+	resource_group_name = azurerm_resource_group.aks_rg.name
+	location            = azurerm_resource_group.aks_rg.location
+	sku                 = var.acr_sku
+	admin_enabled       = false
+
+	tags = var.tags
+}
+
+// Grant AKS cluster pull permissions to ACR (AcrPull role)
+resource "azurerm_role_assignment" "aks_acr_pull" {
+	scope              = azurerm_container_registry.acr.id
+	role_definition_name = "AcrPull"
+	principal_id       = azurerm_user_assigned_identity.aks_identity.principal_id
+}
+
--- InfrastructureAsCode/outputs.tf
+++ InfrastructureAsCode/outputs.tf
@@ -40,3 +40,18 @@ output "identity_principal_id" {
   description = "Principal ID for the user-assigned managed identity"
   value       = azurerm_user_assigned_identity.aks_identity.principal_id
 }
+
+output "acr_id" {
+  description = "ID of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.id
+}
+
+output "acr_login_server" {
+  description = "Login server for the Azure Container Registry"
+  value       = azurerm_container_registry.acr.login_server
+}
+
+output "acr_name" {
+  description = "Name of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.name
+}
